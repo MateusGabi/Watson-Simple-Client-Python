c37f33f7ff1ac58ac9b4935800f95fa9ffb3c107
Mateus Gabi <mateusgabimoreira@gmail.com>
2018-02-25 02:35:49 +0000

    add simple client

---
+++ index.py
@@ -0,0 +1,31 @@
+import json
+import watson_developer_cloud
+import os
+from dotenv import load_dotenv
+
+#
+# configure .env
+load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
+#
+# setting vars
+watson_username = os.environ.get("WATSON_USERNAME")
+watson_password = os.environ.get("WATSON_PASSWORD")
+watson_workspace_id = os.environ.get("WATSON_WORKSPACE_ID")
+
+#
+# setting watson conversation
+conversation = watson_developer_cloud.ConversationV1(
+    username=watson_username,
+    password=watson_password,
+    version='2018-02-16'
+)
+
+response = conversation.message(
+    workspace_id=watson_workspace_id,
+    input={
+        'text': 'manda um suco'
+    }
+)
+
+print(json.dumps(response, indent=2))
